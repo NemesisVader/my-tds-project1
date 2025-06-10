7b4290f5f117503e403b02b4252d6e6463af049a
Divyansh <23f2004203@ds.study.iitm.ac.in>
2025-06-10 11:51:04 +0000

    Enabling Cors for API Fetch

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware  # Import CORSMiddleware
+
+app = FastAPI()
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,            
+    allow_credentials=True,          
+    allow_methods=["*"],              
+    allow_headers=["*"],  
+)
+
 import os
 from dotenv import load_dotenv
 load_dotenv()
